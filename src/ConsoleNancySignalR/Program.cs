c9c4754d9dab8c074975f586fc82813d1bfef908
Louis DeJardin <louis.dejardin@gmail.com>
2012-09-15 22:27:06 -0700

    Adding some comments

--- src/ConsoleNancySignalR/Program.cs
+++ src/ConsoleNancySignalR/Program.cs
@@ -10,15 +10,19 @@ namespace ConsoleNancySignalR
     {
         static void Main(string[] args)
         {
-            var startup = new Startup();
-            var app = new AppBuilder();
+            var startup = new Startup(); // this program's Startup.cs class
+            var app = new AppBuilder(); // the app builder, from Owin.Builder package
 
-            ServerFactory.Initialize(app.Properties);
+            // the server adapter, from Microsoft.HttpListener.Owin package, gets first chance
+            ServerFactory.Initialize(app.Properties); 
             
+            // this method adds all middleware and frameworks
             startup.Configuration(app);
 
-            app.SetHostAddress("http", "+", 8080);   
-         
+            // ext method to set server address in app.Properties
+            app.SetHostAddress("http", "+", 8080);
+
+            // build pipeline, and give to server adapter from Microsoft.HttpListener.Owin package
             using (ServerFactory.Create(app.Build(), app.Properties))
             {
                 Console.WriteLine("Running on http://+:8080/");
